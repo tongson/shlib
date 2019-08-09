006fc737b1b1e9ff15645eaead9c7bdcc8c8387b
Eduardo Tongson <propolice@gmail.com>
2019-08-09 15:35:10 +0800

    No more status messages.

--- 602-systemd.sh
+++ 602-systemd.sh
@@ -1,20 +1,17 @@
 systemd.start()
 {
-    fn.info "Enabling and starting service..."
     systemctl daemon-reload
     systemctl --now enable "$@"
 }
 
 systemd.active()
 {
-    fn.info "Checking if container service is up..."
     systemctl is-active "$@" && return 0
     return 1
 }
 
 systemd.image()
 {
-    fn.info "Generating systemd unit..."
     name=$(cut -f1 -d: <<< "${1}")
     tag=$(cut -f2 -d: <<< "${1}")
     [ "$name" = "$tag" ] && tag="latest"
@@ -24,7 +21,6 @@ systemd.image()
 
 systemd.stop()
 {
-    fn.info "Stopping existing unit..."
     systemctl stop "$@" 2>/dev/null || true
     until ! systemctl is-active --quiet "$@"
     do
@@ -34,13 +30,11 @@ systemd.stop()
 
 systemd.disable()
 {
-    fn.info "Disabling unit ($*)..."
     systemctl disable --now "$@"
 }
 
 systemd.restart()
 {
-    fn.info "Restarting unit ($*)..."
     systemctl daemon-reload
     systemctl restart "$@"
 }
