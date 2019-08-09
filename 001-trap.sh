476d1a3d32490fe6ca2cdc17cae145d1444970de
Eduardo Tongson <propolice@gmail.com>
2019-08-09 15:58:09 +0800

    Proper trap.

--- 001-trap.sh
+++ 001-trap.sh
@@ -1,9 +1,9 @@
-trap '_TRAP_ERROR $? $LINENO "$BASH_COMMAND"' 1 2 3 15 ERR
+trap '_TRAP_ERROR $? $LINENO $BASH_COMMAND; exit' 1 2 3 15 ERR
 
 function _TRAP_ERROR() {
     local err=$1
     local line=$2
     local command="$3"
-    printf "'%s' failed at line %s - exited with status: %s\n" $command" $line $err
+    printf '%s failed at line %s - exited with status: %s\n' $command $line $err
     return $err
 }
