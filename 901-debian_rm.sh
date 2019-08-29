fd2ec8215be6771833fd52d6199c4a6783bda04b
Eduardo Tongson <propolice@gmail.com>
2019-08-29 11:53:24 +0800

    Most of these have errors ignored.

--- 901-debian_rm.sh
+++ 901-debian_rm.sh
@@ -1,17 +1,14 @@
 debian.rmusers()
 {
-    fn.info "Removing useless user accounts..."
     for users in games gnats irc list news sync uucp
     do
         2>/dev/null userdel -r "$users" || true
     done
-    fn.info "Successfully removed accounts."
 }
 
 
 debian.rmsuid()
 {
-    fn.info "Removing suid bits from executables..."
     for p in /bin/fusermount /bin/mount /bin/ping /bin/ping6 /bin/su /bin/umount /usr/bin/bsd-write /usr/bin/chage /usr/bin/chfn /usr/bin/chsh /usr/bin/mlocate /usr/bin/mtr /usr/bin/newgrp /usr/bin/pkexec /usr/bin/traceroute6.iputils /usr/bin/wall /usr/sbin/pppd
     do
         if [ -e "$p" ]
@@ -23,5 +20,4 @@ debian.rmsuid()
             chmod -s $p
         fi
     done
-    fn.info "Successfully removed suid bits."
 }
