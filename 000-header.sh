6746c6d253b012592d013e1566fadd29775e7b72
Eduardo Tongson <propolice@gmail.com>
2019-08-29 11:47:05 +0800

    Export for global usage.

--- 000-header.sh
+++ 000-header.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 unset IFS
 set -o errexit -o nounset -o pipefail -o errtrace
-PATH=/bin:/sbin:/usr/bin:/usr/sbin
-LC_ALL=C
+export PATH=/bin:/sbin:/usr/bin:/usr/sbin
+export LC_ALL=C
