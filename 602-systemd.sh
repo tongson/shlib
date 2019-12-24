fac4a3a5fb76bd0cf4dd6413253b53fb6202bd29
Eduardo Tongson <propolice@gmail.com>
2019-12-24 09:02:33 +0800

    Use posix shell substitution.

--- 602-systemd.sh
+++ 602-systemd.sh
@@ -12,11 +12,11 @@ systemd.active()
 
 systemd.image()
 {
-    name=$(cut -f1 -d: <<< "${1}")
-    tag=$(cut -f2 -d: <<< "${1}")
-    [ "$name" = "$tag" ] && tag="latest"
-    iid=$(/usr/bin/podman images | grep -F -- "/${name} " | grep -F -- " $tag " | awk '{print $3}')
-    sed -i "s|__IMAGE__|$iid|" "/etc/systemd/system/${2}"
+    _name="${1%%:*}"
+    _tag="${1##*:}"
+    [ "$_name" = "$_tag" ] && _tag="latest"
+    _iid=$(/usr/bin/podman images | grep -F -- "/${_name} " | grep -F -- " $_tag " | awk '{print $3}')
+    sed -i "s|__IMAGE__|$_iid|" "/etc/systemd/system/${2}"
 }
 
 systemd.stop()
