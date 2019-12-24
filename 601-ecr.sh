9786b15e158b60536666b14cde6bdf663e9b3532
Eduardo Tongson <propolice@gmail.com>
2019-12-24 09:11:32 +0800

    Still hardcoded.

--- 601-ecr.sh
+++ 601-ecr.sh
@@ -3,6 +3,6 @@ ecr.pull()
     _name="${2%%:*}"
     _tag="${2##*:}"
     [ "$_name" = "$_tag" ] && _tag="latest"
-    /usr/bin/podman login -u AWS -p $(/usr/bin/aws ecr get-login --no-include-email|awk '{print $6}') 872492578903.dkr.ecr.ap-southeast-1.amazonaws.com
+    /usr/bin/podman login -u AWS -p $(/usr/bin/aws ecr get-login --no-include-email|awk '{print $6}') "${1}"
     /usr/bin/podman pull "${1}/${_name}:${_tag}"
 }
