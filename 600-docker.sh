d9687557bb14c7a840b472b4c4b755b42c8480af
Eduardo Tongson <propolice@gmail.com>
2019-12-24 09:11:06 +0800

    For pulling from generic docker registries.

---
+++ 600-docker.sh
@@ -0,0 +1,11 @@
+docker.pull()
+{
+    # docker.pull docker://.... name:tag login:password
+    _name="${2%%:*}"
+    _tag="${2##*:}"
+    _username="${3%%:*}"
+    _password="${3##*:}"
+    [ "$_name" = "$_tag" ] && _tag="latest"
+    /usr/bin/podman login -u "${_username}" -p "${_password}" "${1}"
+    /usr/bin/podman pull "${1}/${_name}:${_tag}"
+}
