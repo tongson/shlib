bbcd3ce83c89ff55d6e178bc9edfd0b410762ecb
Eduardo Tongson <propolice@gmail.com>
2020-07-02 11:35:52 +0800

    Looks natural when left goes to right. URL->FILE.

--- 701-www.sh
+++ 701-www.sh
@@ -4,6 +4,6 @@ curl -H 'Pragma: no-cache' \
      -H 'Cache-Control: no-cache' \
      --tcp-fastopen --tcp-nodelay \
      --retry 32 --retry-delay 1 \
-     -s -L -o "${1}" "${2}"
+     -s -L -o "${2}" "${1}"
 }
 
