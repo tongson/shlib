b3be7c3999b74a4b575cbccdaac38fd079a025b4
Eduardo Tongson <propolice@gmail.com>
2020-07-02 15:12:00 +0800

    Also retry connection refused errors.

--- 701-www.sh
+++ 701-www.sh
@@ -3,6 +3,7 @@ __fetch()
 curl -H 'Pragma: no-cache' \
      -H 'Cache-Control: no-cache' \
      --tcp-fastopen --tcp-nodelay \
+     --retry-connrefused \
      --retry 32 --retry-delay 1 \
      -s -L -o "${2}" "${1}"
 }
