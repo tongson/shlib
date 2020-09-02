2ec483954e2e3ea79701be8f3b6bb421eea600a6
Eduardo Tongson <propolice@gmail.com>
2020-09-02 20:05:30 +0800

    Let PATH deal with it.

--- 601-ecr.sh
+++ 601-ecr.sh
@@ -3,6 +3,6 @@ __ecr_pull()
     _name="${2%%:*}"
     _tag="${2##*:}"
     [ "$_name" = "$_tag" ] && _tag="latest"
-    /usr/bin/podman login -u AWS -p $(/usr/bin/aws ecr get-login --no-include-email|awk '{print $6}') "${1}"
-    /usr/bin/podman pull "${1}/${_name}:${_tag}"
+    podman login -u AWS -p $(/usr/bin/aws ecr get-login --no-include-email|awk '{print $6}') "${1}"
+    podman pull "${1}/${_name}:${_tag}"
 }
