56c11dbbdc62fac727ed2f66c11de6b89c25e27e
Eduardo Tongson <propolice@gmail.com>
2020-09-02 20:03:27 +0800

    Let PATH deal with it.

--- 600-docker.sh
+++ 600-docker.sh
@@ -6,6 +6,6 @@ __podman_pull()
     _username="${3%%:*}"
     _password="${3##*:}"
     [ "$_name" = "$_tag" ] && _tag="latest"
-    /usr/bin/podman login -u "${_username}" -p "${_password}" "${1}"
-    /usr/bin/podman pull "${1}/${_name}:${_tag}"
+    podman login -u "${_username}" -p "${_password}" "${1}"
+    podman pull "${1}/${_name}:${_tag}"
 }
