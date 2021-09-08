b5ccbd7c75d3ded434d826d3e691de16c3c1fa4c
Eduardo Tongson <propolice@gmail.com>
2021-09-08 08:53:01 +0800

    Rename function names and some tweaks.

--- 900-debian.sh
+++ 900-debian.sh
@@ -1,4 +1,4 @@
-__debian_aptget()
+__apt_get()
 {
     /usr/bin/env LC_ALL=C DEBIAN_FRONTEND=noninteractive apt-get -qq \
         --no-install-recommends \
@@ -6,19 +6,21 @@ __debian_aptget()
         -o APT::Get::AutomaticRemove=1 \
         -o Dpkg::Use-Pty=0 \
         -o Dpkg::Options::="--force-unsafe-io" \
+        -o Dpkg::Options::="--force-confdef" \
         -o Dpkg::Options::="--force-confold" "$@"
 }
 
-__debian_purge()
+__apt_purge()
 {
-        dpkg --purge --no-triggers --force-remove-essential --force-breaks --force-unsafe-io "$@"
+        dpkg --purge --no-triggers --force-remove-reinstreq --force-remove-essential --force-breaks --force-unsafe-io "$@"
 }
 
-__debian_upgrade()
+__apt_upgrade()
 {
-    __debian_aptget update &&
-    __debian_aptget full-upgrade &&
-    __debian_aptget ---purge autoremove || :
-    __debian_aptget autoclean || :
+    # shellcheck disable=SC2015
+    __apt_get update &&
+    __apt_get full-upgrade &&
+    __apt_get ---purge autoremove || :
+    __apt_get autoclean || :
     systemctl reboot
 }
