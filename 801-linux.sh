8353d17badbfb5862ce3428a191ffd57d9d6864b
Eduardo Tongson <propolice@gmail.com>
2021-09-08 09:26:20 +0800

    Improve and rename functions. Remove unneeded functions.

--- 801-linux.sh
+++ 801-linux.sh
@@ -1,32 +1,16 @@
-linux.osid()
+__linux_osid()
 {
-    [[ $(awk '(/^ID='${1}'/ || /^ID="'${1}'/ || /^ID=\x27'${1}'/) {n++} END {print n}' /etc/os-release) = "1" ]] && return 0
+  local v
+  v=$(awk '(/^ID='"${1}"'/ || /^ID="'"${1}"'/ || /^ID=\x27'"${1}"'/) {n++} END {print n}' /etc/os-release)
+  if [ "$v" = 1 ]
+  then
+    return 0
+  else
     return 1
+  fi
 }
 
-linux.install()
-{
-    if linux.osid "opensuse"
-    then
-        zypper --non-interactive install --no-recommends "$@"
-    fi
-    if linux.osid "opensuse-tumbleweed"
-    then
-        zypper --non-interactive install --no-recommends "$@"
-    fi
-    if linux.osid "debian"
-    then
-         /usr/bin/env LC_ALL=C DEBIAN_FRONTEND=noninteractive apt-get -qq \
-        --no-install-recommends  \
-        -o APT::Install-Suggests=0  \
-        -o APT::Get::AutomaticRemove=1 \
-        -o Dpkg::Use-Pty=0 \
-        -o Dpkg::Options::='--force-confdef' \
-        -o Dpkg::Options::='--force-confold' "$@"        
-    fi
-}
-
-linux.useradd()
+__linux_useradd()
 {
     if ! getent passwd "${1}"
     then
@@ -36,21 +20,3 @@ linux.useradd()
         chown -R "${2}:${2}" "/home/${1}"
     fi
 }
-
-linux.chown()
-{
-    chown -R "${2}:${2}" "/home/${1}"
-}
-
-linux.keygen() {
-    __tmpdir_linux_keygen=$(mktemp -d -p .)
-    cd "${__tmpdir_linux_keygen}"
-    ssh-keygen -t rsa -b 4096 -P '' -f "$2"
-    mkdir -p "/home/$2/.ssh"
-    mv "$2.pub" "/home/$2/.ssh/$1.pub"
-    mv "$2" "/home/$2/.ssh/$1.rsa"
-    chmod 0600 "/home/$2/.ssh/$1.rsa"
-    cd ..
-    rm -rf "${__tmpdir_linux_keygen}"
-}
-
