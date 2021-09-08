67d4fa7d04bed9d3d283dfc69f497be3f18bdd97
Eduardo Tongson <propolice@gmail.com>
2021-09-08 09:15:33 +0800

    Rename function names and some tweaks.

--- 901-debian_rm.sh
+++ 901-debian_rm.sh
@@ -1,13 +1,13 @@
-debian.rmusers()
+__debian_rmusers()
 {
     for users in games gnats irc list news sync uucp
     do
-        2>/dev/null userdel -r "$users" || true
+        2>/dev/null userdel -r "$users" || :
     done
 }
 
 
-debian.rmsuid()
+__debian_rmsuid()
 {
     for p in /bin/fusermount /bin/mount /bin/ping /bin/ping6 /bin/su /bin/umount /usr/bin/bsd-write /usr/bin/chage /usr/bin/chfn /usr/bin/chsh /usr/bin/mlocate /usr/bin/mtr /usr/bin/newgrp /usr/bin/pkexec /usr/bin/traceroute6.iputils /usr/bin/wall /usr/sbin/pppd
     do
@@ -15,8 +15,8 @@ debian.rmsuid()
         then
             oct=$(stat -c "%a" $p |sed 's/^4/0/')
             ug=$(stat -c "%U %G" $p)
-            2>/dev/null dpkg-statoverride --remove $p || true
-            2>/dev/null dpkg-statoverride --add "$ug" "$oct" $p || true
+            2>/dev/null dpkg-statoverride --remove $p || :
+            2>/dev/null dpkg-statoverride --add "$ug" "$oct" $p || :
             chmod -s $p
         fi
     done
